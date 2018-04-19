69d076536dec3723b2acfe460962b10934ac8726
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2018-04-19 22:07:20 +0200

    started with the program

--- app/src/main/java/com/example/dante/friendsr/MainActivity.java
+++ app/src/main/java/com/example/dante/friendsr/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.dante.friendsr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Friend> friends = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+
+        }
+        else {
+            String[] name_list = {"arya", "cersei", "deanerys", "jon", "jorah", "margaery",
+                                  "melisandre", "sansa", "tyrion"};
+            for (String name : name_list) {
+                String bio = "This is a placeholder for the bio off " + name;
+
+                int drawID = getResources().getIdentifier(name, "id", getPackageName());
+                Friend friend = new Friend(name, bio, drawID);
+                friends.add(friend);
+            }
+
+        }
     }
 }
