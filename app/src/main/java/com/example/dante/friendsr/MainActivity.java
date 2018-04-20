40c0c4f58071eb1c25f6119557f97d3db67f8a23
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2018-04-20 18:37:08 +0200

    added profiles, Now need to make SharedPreferences

--- app/src/main/java/com/example/dante/friendsr/MainActivity.java
+++ app/src/main/java/com/example/dante/friendsr/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dante.friendsr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                                   "melisandre", "sansa", "tyrion"};
             for (String name : name_list) {
                 String bio = "This is a placeholder for the bio off " + name +
-                             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in neque " +
+                             ": Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in neque " +
                              "ac arcu venenatis ornare ut sit amet dolor. Pellentesque quis tellus " +
                              "in ante placerat aliquet. Quisque eu risus eget metus ornare dignissim " +
                              "ac vitae leo. Pellentesque sed sagittis nisl. Donec quis elementum " +
@@ -30,11 +34,27 @@ public class MainActivity extends AppCompatActivity {
                 int drawID = getResources().getIdentifier(name, "drawable", getPackageName());
                 Friend friend = new Friend(name, bio, drawID);
                 friends.add(friend);
+                GridView gv = findViewById(R.id.friends_grid);
+                gv.setOnItemClickListener(new GridItemClickListener());
             }
 
             FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
             GridView friend_grid = findViewById(R.id.friends_grid);
             friend_grid.setAdapter(adapter);
+
+        }
+    }
+
+    private class GridItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Friend clickedFriend = (Friend) parent.getItemAtPosition(position);
+            Log.d("click_tester", "test test " + clickedFriend.getName());
+
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            intent.putExtra("clicked_friend", clickedFriend);
+            startActivity(intent);
 
         }
     }
--- app/src/main/java/com/example/dante/friendsr/ProfileActivity.java
+++ app/src/main/java/com/example/dante/friendsr/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.example.dante.friendsr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        Friend retrievedFriend = (Friend) intent.getSerializableExtra("clicked_friend");
+
+        TextView Name = findViewById(R.id.Name);
+        Name.setText(retrievedFriend.getName());
+
+        TextView Bio = findViewById(R.id.Bio);
+        Bio.setText(retrievedFriend.getBio());
+
+        ImageView img = findViewById(R.id.Picture);
+        img.setImageResource(retrievedFriend.getDrawableId());
+
     }
 }
